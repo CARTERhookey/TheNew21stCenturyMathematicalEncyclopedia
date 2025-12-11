aebd7429dfe3ecd8a048c1e02a8da6e4eb6b8539
CARTERhookey <chookey2025@outlook.com>
2025-12-10 19:06:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-const priceInput = document.getElementById("itemPrice");
-const costInput = document.getElementById("wholesaleCost");
-const qtyInput = document.getElementById("quantitySold");
-const resultEl = document.getElementById("result");
 
-let chart; // this will be our graph later
-
-document.getElementById("calcBtn").addEventListener("click", () => {
-    const price = Number(priceInput.value);
-    const cost = Number(costInput.value);
-    const qty = Number(qtyInput.value);
-
-    const profitPerItem = price - cost;
-    const totalProfit = profitPerItem * qty;
-
-    resultEl.textContent = `Total Profit: $${totalProfit}`;
-
-    drawChart(profitPerItem, qty);
-});
